eea2e16d5a2f9638b2fed11e4888d6ab1a933dee
jemikp <jemikp2000@gmail.com>
2021-06-01 17:47:40 +0530

    Initial Commit

---
+++ api/nodemailer/send-email.js
@@ -0,0 +1,29 @@
+var nodemailer = require('nodemailer');
+
+module.exports.send_email = function(userEmail, time){
+    var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'jemik243patel@gmail.com',
+        pass: 'Jemik@1234'
+    }
+    });
+
+    var mailOptions = {
+    from: 'jemik243patel@gmail.com',
+    to: `${userEmail}`,
+    subject: 'Sending email using nodemailer',
+    text: `Hello sending this from nodemailer
+                "To kaisea hai aap? 
+                
+                Your appointment is confirmed at ${time} `
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+    });
+}
---
+++ api/nodemailer/test.js
@@ -0,0 +1,3 @@
+const email_module = require('./send-email');
+
+email_module.send_email("180200107085@gecrajkot.ac.in", "1:20");
